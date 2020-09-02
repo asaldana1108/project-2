54b3721af650891b5ca1842313868c5abfacaed4
Victor Cesar Lopez <civ187@gmail.com>
2020-09-01 20:49:38 -0500

    updated

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,7 +95,7 @@ router.post('/', (req, res) => {
     Post.create({
       title: req.body.title,
       description: req.body.description,
-      user_id: req.body.user_id
+      user_id: req.session.user_id
     })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
