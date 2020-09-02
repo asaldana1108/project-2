efc468a1ceedf795342a4bc8de8700d5673b2935
asaldana1108 <asaldana1108@gmail.com>
2020-09-02 18:31:16 -0500

    added styling to homepage, dashboard, and edit-post

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const app = express();
-const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 //const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const hbs = exphbs.create({});
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // set up express session and connect session to Sequelize db
 const sess = {
 	secret            : 'super secret',//process.env.SESS_SECRET,
-  // timeout cookie after 1 hr
+	// timeout cookie after 1 hr
 	cookie            : {maxAge: 3600000},
 	resave            : false,
 	saveUninitialized : true,
@@ -24,11 +27,13 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // sequelize.sync({ force: true }).then(() => {
