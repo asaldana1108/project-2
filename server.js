ffa73c935ad256433bfa6d7075e7e2766d42de50
Joey Ramos <joey@Joeys-Mac-mini.local>
2020-09-02 20:41:20 -0500

    helpers

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helperss});
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
