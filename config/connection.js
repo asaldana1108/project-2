4d516832a3cfbf2985f4987da0b3b25a3b7db793
asaldana1108 <asaldana1108@gmail.com>
2020-09-06 19:07:53 -0500

    updating connection.js to connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,8 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -9,3 +14,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+
+module.exports = sequelize;
